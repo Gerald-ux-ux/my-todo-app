9e3dd0415eb36471847ec064e04539a1d85bc8b0
Gerald-ux-ux <11geraldmoney@gmail.com>
2022-11-13 22:24:33 +0300

    adding more style

--- src/Components/AddTaskForm.jsx
+++ src/Components/AddTaskForm.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
   return (
-    <div >
-      <input className="row"
-        type="text"
-        placeholder='Add a task'
-        autoComplete='true'
-        autoCapitalize='true'
-        autofocus="true"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-      />
-      <div className="btn">
-        <button 
-          onClick={addTask}
-        >Add Your Task</button>
+    <>
+      <div className="row">
+        <div className="col">
+          <input 
+            type="text"
+            placeholder='Add a task'
+            autoComplete='true'
+            autoCapitalize='true'
+            autofocus="true"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </div>
+        <div className="btn">
+          <button
+            onClick={addTask}
+          >Add Your Task</button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
